cd17b604204bc7c063a757cd6c6f0a54acad1405
Kharristov <annawey486@gmail.com>
2024-12-05 15:07:08 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static long SquareDigits(long n)
+    {
+        string numStr = n.ToString();  
+        string result = ""; 
+
+        foreach (char digit in numStr)
+        {
+            int digitValue = int.Parse(digit.ToString()); 
+            result += (digitValue * digitValue).ToString();  
+        }
+
+        return long.Parse(result);  
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число для возведения каждой цифры в квадрат:");
+        string input = Console.ReadLine();
+
+        if (long.TryParse(input, out long number))
+        {
+            Console.WriteLine($"Результат: {SquareDigits(number)}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: введено не число.");
+        }
+    }
+}
